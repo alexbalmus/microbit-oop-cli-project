9ac96a9606b40050797ef4b9588af0c9d4ef5e7e
Alexandru-Cristian Balmus <alexbalmus@gmail.com>
2020-08-12 14:13:10 +0300

    Update pxt.json, README.md, main.ts, test.ts

---
+++ main.ts
@@ -0,0 +1,70 @@
+class Greeting {
+    message: string;
+
+    constructor(message: string) {
+        this.message = message;
+    }
+    
+    public getMessage(): string {
+        return this.message;
+    }
+}
+
+let hello = new Greeting("HI WORLD!");
+
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    . # . # .
+    # . . . #
+    . # # # .
+    `);
+
+basic.pause(500);
+basic.clearScreen();
+
+basic.forever(function onForever() {
+    basic.clearScreen();
+    basic.showString("" + hello.getMessage());
+
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `);
+    
+    basic.pause(500);
+    basic.showIcon(IconNames.Heart);
+
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `);
+    
+    basic.showIcon(IconNames.Heart);
+
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `);
+    
+    basic.clearScreen();
+
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `);
+    
+    basic.clearScreen();
+})
